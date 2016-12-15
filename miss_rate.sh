2f76ad7f81e32946ccbad2576bf9b6a8dd51a2ae
namsan <namsan@kaist.ac.kr>
2016-12-15 22:51:31 +0900

    miss_Rate

---
+++ miss_rate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cat adpcm_result.txt |grep dl1.miss_rate
+cat ispell_result.txt |grep dl1.miss_rate
+cat jpeg_result.txt |grep dl1.miss_rate
+cat lame_result.txt |grep dl1.miss_rate
+cat tiff2bw_result.txt |grep dl1.miss_rate
+cat tiffmedian_result.txt |grep dl1.miss_rate
